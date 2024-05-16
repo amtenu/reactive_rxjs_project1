09bcbcf6d56b8634d175fe0610dc2d0ed19c9b70
amtenu <amannov21@gmail.com>
2024-05-15 21:07:53 -0600

    added values and made it async by adding settimeout

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const {Observable}=require("rxjs")
+const { Observable } = require("rxjs");
 
-const observable=new Observable((subscriber)=>{
-subscriber.next(10)
-})
+const observable = new Observable((subscriber) => {
+  subscriber.next(10);
+  subscriber.next(11);
+  subscriber.next(12);
+  setTimeout(() => {
+    subscriber.next(13);
+  }, 2000);
+});
 
-const observer ={
-    next:(value)=>{console.log(`Observer got a value of ${value}`)},
-    error:(error)=>{console.log("Observer got an error of " + error)},
-    complete:()=>{console.log("Observer got complete notificatioin")}
-}
+const observer = {
+  next: (value) => {
+    console.log(`Observer got a value of ${value}`);
+  },
+  error: (error) => {
+    console.log("Observer got an error of " + error);
+  },
+  complete: () => {
+    console.log("Observer got complete notificatioin");
+  },
+};
 
-observable.subscribe(observer)
+observable.subscribe(observer);
